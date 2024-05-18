b41b7cc3f067a0c7a0fc2cf7264523bdf36eada2
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-05-18 13:23:19 -0600

    Updated at 18, may

--- config.js
+++ config.js
@@ -2,10 +2,19 @@ const mysql = require('mysql'); // USar la libreria
 
 // Configuracion de la base de datos
 const config = mysql.createConnection({
-    host: 'santarosadev.xyz', //127.0.0.1 
+    host: '127.0.0.1', //127.0.0.1 
     user: 'root',
-    password: '',
+    password: 'carlos2024',
     database: 'proyectos'
+});
+
+//Verify the connection
+config.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Conectado a la base de datos');
 });
 
 module.exports = config; // Exportar la configuracion
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Incluir el CORS
 const cors = require('cors');
 
+// Incluir el body-parser (Para procesar peticiones POST)
+const bodyParser = require('body-parser');
+
 //Incluir la configuracion de conexion a mysql
 const config = require('./config');
 
@@ -12,6 +15,9 @@ const app = express();
 
 //Usar el CORS
 app.use(cors());
+
+// usar el body-parser para procesar peticiones POST
+app.use(bodyParser.json()); 
 
 //Usar el puerto 3000
 const port = 3000;
@@ -30,6 +36,27 @@ app.get('/proyectos', (req, res) => {
             res.status(500).send('Error al obtener los proyectos');
         } else {
             res.json(filas);
+        }
+    });
+});
+
+//Store a project in the database
+app.post('/proyecto', (req, res) => {
+    // Get the data from the request
+    const data = req.body;
+    console.log(data);
+
+    // Perform the query
+    config.query('INSERT INTO proyectos SET ?', data, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error al guardar el proyecto');
+        } else {
+            // Return the response
+            res.json({
+                id: result.insertId,
+                ...data
+            });
         }
     });
 });
